1780a63ddc0a1378316ff71e625226e2fa6a8c50
jimin yun <ge72946@gmail.com>
2024-01-19 00:07:34 +0900

    라우터 셋업

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from "./screens/Home";
 import Login from "./screens/Login";
-import NotFound from "./screens/NotFound"
+import NotFound from './screens/NotFound';
 
 function App() {
   const isLoggedIn = true;
