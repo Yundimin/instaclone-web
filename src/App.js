798e8c82708be7463b3c8484bf1995ae99b19598
jimin yun <ge72946@gmail.com>
2024-01-20 13:26:25 +0900

    reactive variables

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
+import { useReactiveVar } from "@apollo/client";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 import NotFound from './screens/NotFound';
+import { isLoggedInVar } from "./apollo";
 
 function App() {
-  const isLoggedIn = true;
-  return (
+  const isLoggedIn = useReactiveVar(isLoggedInVar);  return (
     <Router>
       <Switch>
         <Route path="/" exact>
-          {isLoggedIn ? <Home /> : <Login />}
+        {isLoggedIn ? <Home /> : <Login />}
         </Route>
         <Route>
           <NotFound />
