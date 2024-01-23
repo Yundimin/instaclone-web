2b5d42fdfc681b292067ba52b7a766d2b57509c6
jimin yun <ge72946@gmail.com>
2024-01-23 22:45:38 +0900

    Helmet Component

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -3,6 +3,7 @@ import {
   faInstagram,
 } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 import styled from "styled-components";
 import AuthLayout from "../components/auth/AuthLayout";
 import BottomBox from "../components/auth/BottomBox";
@@ -11,6 +12,7 @@ import FormBox from "../components/auth/FormBox";
 import Input from "../components/auth/Input";
 import Separator from "../components/auth/Separator";
 import routes from "../routes";
+import PageTitle from "../components/PageTitle";
 
 const FacebookLogin = styled.div`
   color: #385285;
@@ -23,27 +25,28 @@ const FacebookLogin = styled.div`
 function Login() {
   return (
     <AuthLayout>
-    <FormBox>
-      <div>
-        <FontAwesomeIcon icon={faInstagram} size="3x" />
-      </div>
-      <form>
-        <Input type="text" placeholder="Username" />
-        <Input type="password" placeholder="Password" />
-        <Button type="submit" value="Log in" />
-      </form>
-      <Separator />
-      <FacebookLogin>
-        <FontAwesomeIcon icon={faFacebookSquare} />
-        <span>Log in with Facebook</span>
-      </FacebookLogin>
-    </FormBox>
-    <BottomBox
-      cta="Don't have an account?"
-      linkText="Sign up"
-      link={routes.signUp}
-    />
-  </AuthLayout>
+      <PageTitle title="Login" />
+      <FormBox>
+        <div>
+          <FontAwesomeIcon icon={faInstagram} size="3x" />
+        </div>
+        <form>
+          <Input type="text" placeholder="Username" />
+          <Input type="password" placeholder="Password" />
+          <Button type="submit" value="Log in" />
+        </form>
+        <Separator />
+        <FacebookLogin>
+          <FontAwesomeIcon icon={faFacebookSquare} />
+          <span>Log in with Facebook</span>
+        </FacebookLogin>
+      </FormBox>
+      <BottomBox
+        cta="Don't have an account?"
+        linkText="Sign up"
+        link={routes.signUp}
+      />
+    </AuthLayout>
   );
 }
 export default Login;
